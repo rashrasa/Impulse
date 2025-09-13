e0a3d196fd57abd8cabe68445c61505521e42329
rashrasa <ahmed.rashrash@gmail.com>
2025-09-13 19:53:41 -0400

    created basic window

--- PhysicsSimulations/PhysicsSimulations.cpp
+++ PhysicsSimulations/PhysicsSimulations.cpp
@@ -1,20 +1,43 @@
-
+#define GLFW_INCLUDE_NONE
 #include <iostream>
 #include "pch.h"
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+static void error_callback(int error, const char* description) {
+	std::cerr << "Error: " << description << std::endl;
+}
+
 int main() {
+	// Initialize
+	if (!glfwInit()) {
+		throw "Could not initialize GLFW";
+	}
+	glfwSetErrorCallback(error_callback);
+	auto window = glfwCreateWindow(640, 480, "Physics Simulations", NULL, NULL);
+	if (!window) {
+		throw "Could not create window";
+	}
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
 	PhysicsEngine::World world;
 	PhysicsEngine::RectangularPrism rectangle;
 
+	// Function body
 	rectangle.position << 1.0, 1.0, 1.0;
 	rectangle.velocity << 0.0, 0.0, 0.0;
 	rectangle.acceleration << 0.0, 0.0, 0.0;
 	rectangle.width = 30.0;
 	rectangle.length = 90.0;
-
 	std::cout << rectangle.position.x() << "\n";
-
 	world.entities.push_back(&rectangle);
 
+	// Main event loop
+	while (!glfwWindowShouldClose(window)) {
+
+	}
+
+	// Dispose
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
